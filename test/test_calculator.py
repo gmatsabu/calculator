82cb3a11f3594236ec61de2a1fb3eed6254908b5
gmatsabu <60338174+gmatsabu@users.noreply.github.com>
2020-02-25 08:21:06 +0200

    Update test_calculator.py

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -7,3 +7,9 @@ def test_add_two_numbers():
 
 def test_add_many_numbers():
     assert calculator.add(1,2,3,4,5) == 15
+
+def test_multiply_two_numbers():
+    assert multiply(1,3) == 3
+
+def test_multiply_many_numbers():
+    assert multiply(1,2,3,4,5) == 120    
